dad2a03cc326d60c4d1d43ca6e48498ba65ebaf4
Finn Desktop <finnj00@gmail.com>
2024-09-22 21:21:39 -0700

    Started work on password scanner

---
+++ login_scanner.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import re
+import argparse
+from collections import defaultdict
+
+def scan_login(filename):
+    failedAttempts = defaultdict(int)
+    with open(filename) as logs:
+        for line in logs:
+            match = re.search(r'Failed password for (invalid user )?(.*)from (\d+.\d+.\d+.\d+) port (\d+)', line)
+            if match:
+                print(match.group())
+                print(match.group(2))
+                print(match.group(3))
+                print(match.group(4))
+            pass
+
+def main():
+    parser = argparse.ArgumentParser(description='Script to check log files and  report any suspicious login attempts (more than 10 failed)')
+    parser.add_argument('filename', type=str, help='Name of login log file')
+    args = parser.parse_args()
+    scan_login(args.filename)
+
+if __name__ == "__main__":
+    main()
